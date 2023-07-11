08f0eada36b2d2b258afad0f6b467b2b9e44f1a2
Ayush Kumar <aayushsinghk84@gmail.com>
2023-07-11 22:55:11 +0530

    Intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '22aa1459-77d2-4036-ad2d-f17f982ab497';
+export const MODAL_CLOSE_SEC = 2.5;
